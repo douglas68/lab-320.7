0ae3293e7da469ee99718414775971bc3253026b
Ashley Domineck <adouglas0721@gmail.com>
2025-09-30 08:12:13 -0400

    repo2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState, useEffect} from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+  const apiKey = "98e3fb1f";
+
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async(searchTerm) => {
+	try {
+		const response = await fetch(
+			`http://www.omdbapi.com/?i=tt3896198&apikey=fe6ecc2d`
+		);
+		const data = await response.json();
+		setMovie(data);
+	} catch(e) {
+		console.error(e)
+	}
 }
 
-export default App
+  // This will run on the first render but not on subsquent renders
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
+
+  return (
+    <div className="App">
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
+    </div>
+  );
+}
